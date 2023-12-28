6c78edef8b4b89ec7adcb913668cc8eb6229aef7
yousef <yousefahmedelshahed@gmail.com>
2023-12-28 18:11:00 +0200

    add react-slick-css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 import { Provider } from "react-redux";
 import { store } from "@/redux/store";
 import { Toaster } from "react-hot-toast";
